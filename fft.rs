302f83749c6e260d24f258145da1de7d5f33bd55
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-03-20 11:06:20 +0100

    Update fft.rs

--- fft.rs
+++ fft.rs
@@ -1,22 +1,37 @@
+use std::string;
+
 #[derive(Debug)]
-struct ImgMat {
+struct ImgVec {
     pixels: [u32; 256*256],
-    size: usize
+    stride: usize
 }
 
-impl ImgMat {
+impl ImgVec {
 
-    pub fn new() -> ImgMat {
-        ImgMat{pixels: [0; 256*256], size: 256}
+    pub fn new() -> ImgVec {
+        ImgVec{pixels: [0; 256*256], stride: 256}
     }
 
-    pub fn draw_cricle(&mut self, r: usize, cx: usize, cy: usize) {
-        assert!(cx <= self.size-1, "ERROR: cx too big for this img")
-        assert!(cy <= self.size-1, "ERROR: cy too big for this img")
+    pub fn draw_cricle(&mut self, r: usize, cx: usize, cy: usize, color: u32) {
+        let x0 = cx-r;
+        let y0 = cy-r;
+        
+        for i in y0..cy+r {
+            for j in x0..cx+r {
+                let index = j*self.stride+i;
+                if 0 <= index && index <= 256*256-1 {
+                    self.pixels[index] = color;
+                }
+            }
+        }
+    }
+
+    pub fn dump_bmp(&self, file_name: String) {
+    
     }
 }
 
 fn main() {
-    let mut im = ImgMat::new();
-    im.draw_cricle(8, 128, 128);
+    let mut im = ImgVec::new();
+    im.draw_cricle(8, 128, 128, 0x181818FF);
 }
