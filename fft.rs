2f4fcf3dbe9efba3aa0df5f88b1fb8faad18614b
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-04-17 15:15:31 +0200

    Update fft.rs

--- fft.rs
+++ fft.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 
 #[derive(Debug)]
 struct ImgVec {
-    pixels: [u32; 256*256],
+    pixels: [u32; IMG_SZ],
     stride: usize
 }
 
@@ -17,7 +17,7 @@ struct ComplexNum {
 impl ImgVec {
 
     pub fn new() -> ImgVec {
-        ImgVec{pixels: [0; 256*256], stride: 256}
+        ImgVec{pixels: [0; IMG_SZ], stride: IMG_STRIDE}
     }
 
     pub fn draw_square(&mut self, r: usize, cx: usize, cy: usize, color: u32) {
@@ -27,7 +27,7 @@ impl ImgVec {
         for i in y0..cy+r {
             for j in x0..cx+r {
                 let index = j*self.stride+i;
-                if index <= 256*256-1 {
+                if index <= IMG_SZ-1 {
                     self.pixels[index] = color;
                 }
             }
@@ -48,7 +48,7 @@ impl ImgVec {
         bwiter.write(b" \n")?;
         
         // Write image
-        for i in 0..self.stride*self.stride {
+        for i in 0..IMG_SZ {
             let r = (self.pixels[i] >> 8*0) & 0x000000FF as u32;
             let g = (self.pixels[i] >> 8*1) & 0x000000FF as u32;
             let b = (self.pixels[i] >> 8*2) & 0x000000FF as u32;
@@ -63,37 +63,29 @@ impl ImgVec {
         Ok(())
     }
     
-    // TODO is this heap or stack?
-    pub fn rgb2gray(&self) -> [f32; 256*256] {
-        let mut gray_pixels: [f32; 256*256] = [0.0; 256*256];
-        for i in 0..self.stride*self.stride {
-            let r = (self.pixels[i] >> 8*0) & 0x000000FF as u32;
-            let g = (self.pixels[i] >> 8*1) & 0x000000FF as u32;
-            let b = (self.pixels[i] >> 8*2) & 0x000000FF as u32;
-            let fr = r as f32;
-            let fg = g as f32;
-            let fb = b as f32;
-            gray_pixels[i] = 0.299*fr + 0.587*fg + 0.114*fb;
+    pub fn rgb2gray(&self) -> [f32; IMG_SZ] {
+        let mut gray_pixels: [f32; IMG_SZ] = [0.0; IMG_SZ];
+        for i in 0..IMG_SZ {
+            let r = ((self.pixels[i] >> 8*0) & 0x000000FF as u32) as f32;
+            let g = ((self.pixels[i] >> 8*1) & 0x000000FF as u32) as f32;
+            let b = ((self.pixels[i] >> 8*2) & 0x000000FF as u32) as f32;
+            gray_pixels[i] = 0.299*r + 0.587*g + 0.114*b;
         }
         gray_pixels
     }
-    
-    pub fn ft(&self) {
-
-    }
-
 }
+
+fn ft(pixels: &[f32; IMG_SZ]) {
+    println!("OK")
+}
+
+const IMG_SZ: usize = 256*256;
+const IMG_STRIDE: usize = 256;
 
 fn main() {
     let mut im = ImgVec::new();
     im.draw_square(8, 128, 128, 0x181818FF);
     im.dump_bmp("test.ppm".to_string()).unwrap();
-    let gray_p: [f32; 256*256] = im.rgb2gray();
-    for i in 0..255 {
-        for j in 0..255 {
-            let mut index = j*256+i;
-            print!("{} ", gray_p[index])
-        }
-        println!("")
-    }
+    let gray_p: [f32; IMG_SZ] = im.rgb2gray();
+    ft(&gray_p);
 }
